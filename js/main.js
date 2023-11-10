feb13b983d1ea86b6452a88a62667cd2d8cdf051
Tai Phan <ptai7722@gmail.com>
2023-11-10 14:59:32 -0600

    Added a fetch template to begin writing JavaScript for the app

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Fetch API Template
+// fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
+//     .then(res => res.json())
+//     .then(data => {
+//       console.log(data.drinks[0]);
+//       cocktailName.innerText = `Name: ${data.drinks[0].strDrink}`;
+//       cocktailImg.src = data.drinks[0].strDrinkThumb;
+//       cocktailInstructions.innerText = `Instructions: ${data.drinks[0].strInstructions}`
+//       cocktailId = data.drinks[0].idDrink;
+//     })
+//     .catch(err => console.log(`Error! ${err}`));
+
