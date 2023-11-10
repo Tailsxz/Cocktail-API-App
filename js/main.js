671ba9d6284499ec315f0a9849ab0df0696a8839
Tai Phan <ptai7722@gmail.com>
2023-11-10 15:27:20 -0600

    Grabbed all the DOM elements we need within our Cocktail Card constructor

--- js/main.js
+++ js/main.js
@@ -10,3 +10,15 @@ fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
     .catch(err => console.log(`Error! ${err}`));
 //We can see that the ingredients to the cocktail are presented as individual properties with an integer number representing each one starting from 1, max of 15.
 //Knowing this we can create a variable to hold the ingredients string, or we can create a list within our card and insert child list items to that list for each ingredient. With the latter option being a bit more difficult, but might present a nice challenge.
+//lets try an OOP approach to this program
+function CocktailCard() {
+//Lets first grab all the DOM elements we need
+this.cocktail = document.querySelector('#cocktail_input').value;
+this.searchButton = document.querySelector('#cocktail_search');
+this.name = document.querySelector('#cocktail_name');
+this.type = document.querySelector('#cocktail_type');
+this.image = document.querySelector('#cocktail_image');
+this.instructions = document.querySelector('#cocktail_instructions');
+}
+
+let cocktailCard = new CocktailCard();
