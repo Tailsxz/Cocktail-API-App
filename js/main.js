4793f0dbec933b8cfa7be45d117f8ca2f3a77189
Tai Phan <ptai7722@gmail.com>
2023-11-11 20:37:24 -0600

    added error handling

--- js/main.js
+++ js/main.js
@@ -13,35 +13,44 @@
 //lets try an OOP approach to this program
 function CocktailCard() {
 //global intervalId to clear it when a new fetch is sent
+
 let intervalId;
 
 //Lets first grab all the DOM elements we need
+
 const searchButton = document.querySelector('#cocktail_search');
 const cocktailName = document.querySelector('#cocktail_name');
 const type = document.querySelector('#cocktail_type');
 const image = document.querySelector('#cocktail_image');
 const instructions = document.querySelector('#cocktail_instructions');
 const ingredientsUl = document.querySelector('#cocktail_ingredients');
-const cocktailInfoCard = document.querySelector('.card_cocktail');
+const cocktailCard = document.querySelector('.card_cocktail');
+const errorMsg = document.querySelector('#error-message');
+const errorCard = document.querySelector('.card_error');
 //Setting up a method to print all properties to the DOM
+
 this.printCard = function() {
     this.cocktail = document.querySelector('#cocktail_input').value;
     if (!this.cocktail) {
         this.cocktail = prompt('Please enter a valid cocktail');
     }
     fetchAll(this.cocktail);
-    cocktailInfoCard.style = 'display: block;';
+    cocktailCard.style = 'display: block;';
+    errorCard.style = 'display: none;';
 };
 
 //lets set up and initialize an event listener to grab the value of the cocktail the user inputted
+
 function setClickEvent(element, func) {
     element.addEventListener('click', func);
 }
 
 setClickEvent(searchButton, this.printCard)
+
 //we need a reset() function to reset everything each time the search button is pressed
 
 //Lets set up the fetch function
+
 function fetchAll(cocktail) {
     let index = 0
     //in the case the user fetches another list of drinks, clear the current interval, and apply the new one.
@@ -58,9 +67,11 @@ function fetchAll(cocktail) {
             applyAll(data, index);
         }, 7000);
     })
-    .catch(err => cocktailName.innerText = `${cocktail} not found.`);
+    .catch(err => displayError(err));
 }
+
 //seperate function to apply dom manipulation after the data has been fetched
+
 function applyAll(cocktailObj, index) {
     //this conditional will wipe the ingredients for each search the user inputs.
     if (ingredientsUl.innerHTML !== '') {
@@ -71,7 +82,7 @@ function applyAll(cocktailObj, index) {
     type.innerText = currentCocktail.strAlcoholic;
     console.log(currentCocktail)
     image.src = currentCocktail.strDrinkThumb;
-    //inputting the instructions while replacing end of sentence with a line break.
+    //inputting the instructions while replacing end of sentence with a line break. Might turn instructions into an ol.
     instructions.innerText = (currentCocktail.strInstructions.replaceAll('. ', '.\n'));
     setIngredients(currentCocktail);
 }
@@ -87,6 +98,16 @@ function setIngredients(curCocktail) {
         ingredientsUl.appendChild(li).innerHTML = `<span>${currentIngredient}${currentMeasurement ? ` - ${currentMeasurement}` : ``}</span>`;
     }
 }
+
+//We need a function to handle an error state when the user inputs a cocktail that cannot be fetched.
+function displayError(error){
+    //We will need our error function to hide the cocktail card and show the error card, with the error message.
+    cocktailCard.style = 'display: none;';
+    errorCard.style = 'display: block;';
+    errorMsg.innerText = `Drink not found :(`
+    console.log(error);
+}
+
 //lets pseudocode an automatic carousel, we know that we can use modulus to our advantage, to be able to increment the current index every few seconds (setTimeout()), but our fetch and apply function then needs to also run every few seconds with the new index.
 }
 
